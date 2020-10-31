9f4dee5d09b8caeb6da99b1c123b6bf212d5597a
Dilberovicka31 <dilberovicm36@gmail.com>
2020-10-30 20:14:28 -0700

    readme file

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # JS-QUIZ
 JAVASCRIPT QUIZ
+
+* Quiz is compiled of multiple choice questions questions with time limits.
+* Questions are based on Javascript fundamentals to help with testing your knowledge.
+
+
+# Game rules
+
+* Clicking the start button initiates the quiz and starts the timer starts
+* You are presented with a question and if answered correctly 
+, another questions is presented.*If answered incorrectly 10 seconds is subtracted from the clock.
+* After all questions are answered, the timer reaches 0 and game is over.
+* At the end put your initials to save the score.
+
+# CODE : 
+
+
+# Deployed application link : 
+
+
+
---
+++ style.js
@@ -0,0 +1,23 @@
+//setInterval to do the timed functionality 
+
+//clear interval to stop the timeout
+
+// VAR CurrentScore /timeleft
+
+//Question to loop over, arrays 
+
+//Var pointer/index - Current position in the question  array index will be set ot 0 to start ques 1. 
+//event delegation to listen to clicks, == pointer global index and then recall next question 
+//VAR QUESTION  = {
+   // {
+    //"Coding related questions"
+                //List of answers 
+                //sub array in object
+                //correct answer
+                //compare property for right answer
+   // }
+//}
+
+//display and hide elements 
+//
+
