4b17e6fd9b83bc2c0c4eb6188796cbfa265a153c
Dilberovicka31 <dilberovicm36@gmail.com>
2020-11-03 10:02:32 -0800

    added function to render questions

--- script.js
+++ script.js
@@ -21,8 +21,6 @@ function startQuiz(){
    
 
 }
-
-
 function renderQuestions(){
    var questionsIndexLength = questions.length - 1;
    if (index <= questionsIndexLength) {
@@ -30,66 +28,7 @@ function renderQuestions(){
       renderQuestionChoices();
    }
 
-}
-function renderQuestionChoices(){
-      var question = questions[index].choices;
-      for( var option = 0; option < question.length;option++) {
-         var questionOptionDiv = document.getElementById("question-choices");
-         var questionButton = document.createElement("button");
-         questionButton.className = "btn btn-primary";
-         questionButton.innerHTML = question[option];
-         questionButton.setAttribute(
-            "onclick", 
-            "checkAnswer(" + index + "," + option + ");"
-        );
 
-
-         questionOptionDiv.append(questionButton);
-      }
-}
-function checkAnswer(question, answer) {
-   let correctAnswer = questions[question].answer;
-   let userAnswer = questions[question].choices[answer];
-   if (userAnswer == correctAnswer) {
-       index = index + 1;
-   }
-   
-   else {
-       index = index + 1;
-       score = score - 15;
-       console.log(score);
-       console.log("Next question: ", index);
-       console.log("Incorrect");
-   }
-   clearQuestionDiv()
-   renderQuestions();
-}
-
-   function clearQuestionDiv() {
-      document.getElementById("question-choices").innerHTML = "";
-  }
-
-
-
-function timeStart () {
-   var timerEl = setInterval(function() {
-      countdown--;
-      timer.textContent = countdown+ " Seconds left before the quiz ends ";
-      if (countdown === 0){
-         clearInterval(timerEl);
-      }
-
-
-   },1000)
-   
-}
-
-
-
-
-
-
-   
 
 
 //set starting score and start the interval to begin the startdown
